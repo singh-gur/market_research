3ca2f78e585a9d0d0581e9705b957ce1384ce3db
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2025-08-08 14:19:34 -0400

    added yf scraper

--- src/market_research/crew.py
+++ src/market_research/crew.py
@@ -2,6 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+from .tools.yahoo_news_scraper import YahooNewsScraperTool
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
@@ -23,7 +24,8 @@ class MarketResearch():
     def market_news_scraper(self) -> Agent:
         return Agent(
             config=self.agents_config['market_news_scraper'], # type: ignore[index]
-            verbose=True
+            verbose=True,
+            tools=[YahooNewsScraperTool()]
         )
 
     @agent
